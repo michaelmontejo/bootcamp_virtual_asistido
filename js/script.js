b6310325417981e5fbb6d9c6a25984df9c2f79e5
michaelmontejo <michaelmontejoc@gmail.com>
2022-07-16 10:57:08 -0500

    sumas y datos

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
+nombre = prompt("ingresa tu nombre") // string
+edad =  parseInt(prompt("ingresa tu edad") )  // int
+salario = 2500.50 // float
+numero = 20
+hijos = false // booleano
+cedula = "566542" // 
+celular = "3102502556"
+
+suma= parseInt(edad) + numero
+
+
+console.log(nombre, edad, suma)
+
 alert("error 2050")
 console.log("prueba")
